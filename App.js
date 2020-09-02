e7ab3bf8150810e88b1e6f8abae58000688fbea6
NateTheDev1 <nthnlrichards@gmail.com>
2020-09-02 02:09:37 -0400

    Basic rendering and deleting with touchableopac

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
     { name: "nate", key: 1 },
     { name: "eliza", key: 2 },
+    { name: "eliza", key: 3 },
   ]);
 
-  return <View style={styles.container}></View>;
+  const pressHandler = (id) => {
+    setPeople(people.filter((person) => person.key !== id));
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        numColumns={2}
+        style={{ marginTop: 100 }}
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <Text
+              style={{
+                marginHorizontal: 10,
+                marginTop: 24,
+                fontSize: 25,
+                width: 100,
+                height: 50,
+                textAlign: "center",
+                backgroundColor: "gray",
+                color: "white",
+              }}
+            >
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
